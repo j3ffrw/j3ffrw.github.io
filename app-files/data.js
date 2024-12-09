28b9166d315adc46154d16df7d25167713a5d194
jefffermo@gmail.com <jefffermo@gmail.com>
2024-12-10 02:14:01 +0800

    rebuilding site Tuesday, 10 December, 2024 02:14:01 AM PST

---
+++ app-files/data.js
@@ -0,0 +1,471 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frontgate_entrance",
+      "name": "frontgate_entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.3319497591146483,
+        "pitch": -0.13471837418354937,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.15752031394405464,
+          "pitch": -0.006555720641822305,
+          "title": "East",
+          "text": "East"
+        },
+        {
+          "yaw": -2.1066295063777787,
+          "pitch": 0.019568198344481758,
+          "title": "North",
+          "text": "North"
+        },
+        {
+          "yaw": 1.605487827215617,
+          "pitch": 0.027778379546901988,
+          "title": "South",
+          "text": "South"
+        },
+        {
+          "yaw": 2.638864980625965,
+          "pitch": 0.04137328441569821,
+          "title": "West",
+          "text": "West"
+        }
+      ]
+    },
+    {
+      "id": "1-living_room",
+      "name": "living_room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.123254334465173,
+        "pitch": 0.17364265515337252,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.004944386844634607,
+          "pitch": 0.0800713363982446,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "2-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.5317767818144752,
+        "pitch": 0.2467972961046101,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.000676512656417,
+          "pitch": 0.05089234278567645,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "3-restroom_ground",
+      "name": "restroom_ground",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.990725021364181,
+        "pitch": 0.5870898357319767,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-stairs_mid_landing",
+      "name": "stairs_mid_landing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.10245046817165,
+        "pitch": 0.29914823770255694,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.364279721072851,
+          "pitch": 0.00395881750687721,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "5-2ndflr_landing",
+      "name": "2ndFlr_landing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.5868924220252056,
+        "pitch": 0.7310435058856051,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.09923885809125821,
+          "pitch": -0.029620493193814212,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "6-2ndflr_room1",
+      "name": "2ndFlr_room1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.383045709453812,
+        "pitch": 0.35897788524305696,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.053625878469446775,
+          "pitch": 0.12712074066222456,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "7-2ndflr_restroom",
+      "name": "2ndFlr_restroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.9158344943045478,
+        "pitch": 0.583339063519972,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-2ndflr_room2",
+      "name": "2ndFlr_room2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.6766504007258298,
+        "pitch": 0.33841144390101263,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.03996860060934182,
+          "pitch": 0.03758354093939964,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "9-gndflr_parking_rearleft",
+      "name": "gndFlr_parking_rearLeft",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.03551431360075341,
+          "pitch": -0.024406110173153195,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "10-gndflr_parking_frontleft",
+      "name": "gndFlr_parking_frontLeft",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.8404431628834264,
+        "pitch": 0.27297276690357286,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.099573567155538,
+          "pitch": 0.024386444111264893,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    },
+    {
+      "id": "11-gndflr_parking_rearright",
+      "name": "gndFlr_parking_rearRight",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.1237174125428222,
+          "pitch": 0.0132208568367993,
+          "title": "East",
+          "text": "East"
+        }
+      ]
+    }
+  ],
+  "name": "Garnet_140sqmtr_lot",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
